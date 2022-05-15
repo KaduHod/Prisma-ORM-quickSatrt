a9221fd19c320c8f497f251edd52a8636b934dd1
Carlos <carlosjr.ribas@gmail.com>
2022-05-15 10:53:08 -0300

    Ajeitando

--- index.js
+++ index.js
@@ -2,17 +2,11 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-
 let allClients = async () => { return await prisma.clientes.findMany() }
 
-let insertClient = async ( clientAttributes ) => { 
-    return await prisma.clientes.create( clientAttributes )
-} 
+let insertClient = async ( clientAttributes ) => { return await prisma.clientes.create( clientAttributes ) } 
 
-let updateClient = async ( clientOptions ) => {
-    return await prisma.clientes.update( clientOptions )
-}
-
+let updateClient = async ( clientOptions ) => { return await prisma.clientes.update( clientOptions )}
 
 async function main(){
        
@@ -42,55 +36,3 @@ async function main(){
 }
 
 main()
-
-
-
-/* async function main () {
-   /*  const update = await querys.updateClient({
-        where : { id: 2 },
-        data  : { nome: 'Carlos Update' }
-    }) 
-
-    const insert = await querys.insert({
-        nome  : 'oi',
-        email : 'oi',
-        idade : 1,
-        associado: false,
-        estado: 'parana',
-        cidade: 'curitiba',
-        telefone: '09217880234021'
-    })
-    console.log(insert)
-}
-
-main() */
-
-/* (async function(){
-    try { 
-        
-        const { insertClient, updateClient } = querys    
-
-        const clientUpdateOptions = {
-            where : { id: 2 },
-            data  : { nome: 'Carlos Update' }
-        }
-
-        const insert = await insertClient( {
-            data  : {
-                nome  : 'oi',
-                email : 'oi',
-                idade : 1,
-                associado: false,
-                estado: 'parana',
-                cidade: 'curitiba',
-                telefone: '09217880234021'
-        }
-        } )
-
-        console.log('cliente inserido', insert)        
-        
-    } catch (error) {
-        await prisma.$disconnect()
-    }
-    
-})()  */
